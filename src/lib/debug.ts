3b0220b07e36a550e75867bb2318bd7d56c2b50a
Cade <cadeduncan72@gmail.com>
2026-04-09 01:34:42 -0700

    chore: removed Notes.txt from repo & added debug.error

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -3,7 +3,12 @@ const isDev = process.env.NODE_ENV === 'development'
 export const debug = {
   log: (...args: unknown[]) => {
     if (isDev) {
-      console.warn('[debug]', ...args)
-    } // eslint-disable-line no-console
+      console.warn('[debug]', ...args) // eslint-disable-line no-console
+    }
+  },
+  error: (...args: unknown[]) => {
+    if (isDev) {
+      console.error('[debug]', ...args) // eslint-disable-line no-console
+    }
   },
 }
